2e12bb0fe7edd1b07aad8c3404fbe93adb701ecf
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-04-03 11:49:20 +0200

    reordered dir

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,9 @@
-from Fabric import Connection, task
+from fabric import Connection, task
 
 hostnames = ['isabelle-b', 'isabelle-c', 'isabelle-d', 'isabelle-e', 'isabelle-f', 'isabelle-g', 'isabelle-h', 'isabelle-i', 'isabelle-j']
 
 @task
-def deploy_actorsystems(c):
+def deploy(c):
     result = c.run('hostname')
     print(result)
     for hostname in hostnames:
