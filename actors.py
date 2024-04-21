1180b22bf7833cbc749e4c0ebe32d368fe528034
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-04-21 22:23:45 +0200

    implemented creating actor at specific actorsystem

--- actors.py
+++ actors.py
@@ -1,5 +1,16 @@
 import logging
-from thespian.actors import Actor, ActorSystem, ActorSystemConventionUpdate
+from thespian.actors import Actor, ActorSystem, ActorSystemConventionUpdate, requireCapability
+import socket
+
+def get_my_ip():
+    """Return the ipaddress of the local host"""
+    return socket.gethostbyname(socket.gethostname())
+
+@requireCapability("sensor")
+class Ping(Actor):
+
+    def receiveMessage(self, msg, sender):
+        self.send(sender, f"Ping from {get_my_ip()}")
 
 class RegistrationActor(Actor):
 
@@ -9,6 +20,11 @@ class RegistrationActor(Actor):
             self.notifyOnSystemRegistrationChanges()
             logging.info("started registereing")
         if isinstance(msg, ActorSystemConventionUpdate):
-            logging.info(msg)
+            logging.info(msg.remoteAdminAddress)
+            logging.info(msg=msg.remoteCapabilities)
+            logging.info(msg.remoteAdded)
+            if msg.remoteAdded and msg.remoteCapabilities.get('isabelle-b'):
+                remote_actor = self.createActor(Ping, {"isabelle-b": True})
+                self.send(remote_actor, "Ping")
         else:
             logging.info(msg)
--- fabfile.py
+++ fabfile.py
@@ -1,23 +1,29 @@
 from fabric import Connection, task
+from time import sleep
 
 hostnames = ['isabelle-b', 'isabelle-c', 'isabelle-d', 'isabelle-e', 'isabelle-f', 'isabelle-g', 'isabelle-h', 'isabelle-i', 'isabelle-j']
 path = "cd bachelorproef/bachelorproef--actorsystem_deployment_tool"
 
 @task
 def deploy(c):
+    open("pingpong.log", "w").close()
     result = c.run('hostname')
     c.run("tmux new-session -d 'python3 main.py 1900'", pty= False)
-    print(result)
+    sleep(2)
     for hostname in hostnames:
         connection = Connection(hostname)
         with connection.prefix(path):
             result = connection.run("hostname")
-            connection.run("tmux new-session -d 'python3 start_actorsystem.py 1900'", pty=False)
+            capabilities = f'\"{hostname}, sensor\"'
+            connection.run(f"tmux new-session -d 'python3 start_actorsystem.py 1900 {capabilities}'", pty=False)
 
 @task
 def retract(c):
     for hostname in hostnames:
         connection = Connection(hostname)
         with connection.prefix(path):
-            connection.run("hostname")
             connection.run("python3 stop_actorsystem.py 1900", pty=False)
+            connection.run(f"echo \"closed {hostname}\"")
+    hostname = c.run("hostname", hide=True).stdout.strip()
+    c.run("tmux new-session -d 'python3 stop_actorsystem.py 1900'", pty= False)
+    c.run(f"echo \"closed {hostname}\"")
